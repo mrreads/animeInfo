9e8da3ac98c3f632ad30e74ee8c856035aa1fe9a
Alexander <mrreads@yandex.com>
2020-03-31 14:55:40 +0300

    add methot  to get info

--- js/main.js
+++ js/main.js
@@ -20,7 +20,21 @@
         this.request.send();
     }
 
-    
+    getAnime(id)
+    {
+        this.request.open('GET', `https://api.jikan.moe/v3/anime/${id}`);
+
+        this.request.onreadystatechange = function () 
+        {
+            if (this.readyState === 4) 
+            {
+                console.log(JSON.parse(this.responseText));
+                return JSON.parse(this.responseText);
+            }
+        };
+
+        this.request.send();
+    }
 }
 
 let animeInfo = new Handler();
