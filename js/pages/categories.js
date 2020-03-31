33dc65c47cd6f5da6d2fbdf01b69b8af6702f60b
Alexander <mrreads@yandex.com>
2020-03-31 20:55:45 +0300

    category page add | restructure js code

---
+++ js/pages/categories.js
@@ -0,0 +1,45 @@
+﻿let categories = document.querySelector('.categories');
+
+let request = new XMLHttpRequest();
+
+let anime, temp;
+
+function renderCategory(genre_id)
+{
+    request.open('GET', `https://api.jikan.moe/v3/genre/anime/${genre_id}/1`, true);
+    
+    request.onload = () => 
+    {
+        
+        categories.innerHTML = categories.innerHTML + `<h3> ${ JSON.parse(request.responseText)["mal_url"].name } </h3>`;
+        temp = document.createElement('div');
+        temp.classList.add('search-result');
+
+        data = JSON.parse(request.responseText)["anime"];
+
+        data.forEach(anime =>
+        {
+            temp.innerHTML = temp.innerHTML +
+            `
+            <div class="anime">
+                <img src="${ anime.image_url }">
+                <h2> ${ anime.title } </h2>
+                <p> Episodes: ${ anime.episodes }</p>
+            </div>
+            `;  
+        });
+        
+        categories.append(temp);
+
+        if (genre_id != 7)
+        {
+            
+            renderCategory(genre_id + 1)
+        }
+        
+    }
+    
+    request.send();
+}
+
+renderCategory(1);
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,7 +1,29 @@
 ﻿let searchInput = document.querySelector('input#search');
 let searchResult = document.querySelector('.search-result');
 
+let request = new XMLHttpRequest();
+
 searchInput.addEventListener('input', () =>
 {
-    animeInfo.searchAnime(`${searchInput.value}`, searchResult);
+    request.open('GET', `https://api.jikan.moe/v3/search/anime?q=${searchInput.value}&page=1`, true);
+
+    request.onload = () => 
+    {        
+        data = JSON.parse(request.responseText)["results"];
+
+        searchResult.innerHTML = '';
+        data.forEach(anime =>
+        {
+            searchResult.innerHTML = searchResult.innerHTML + 
+            `
+            <div class="anime">
+                <img src="${ anime.image_url }">
+                <h2> ${ anime.title } </h2>
+                <p> Episodes: ${ anime.episodes }</p>
+            </div>
+            `;                
+        });
+    }
+
+    request.send();
 });
