2446126b8672500299d07579e955139c0a19139d
Alexander <mrreads@yandex.com>
2020-03-31 14:51:15 +0300

    added base class | search anime method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-﻿
+﻿class Handler
+{
+    constructor(params) 
+    {
+        this.request = new XMLHttpRequest();
+    }
+
+    searchAnime(name, page = 1)
+    {
+        this.request.open('GET', `https://api.jikan.moe/v3/search/anime?q=${name}&page=${page}`);
+
+        this.request.onreadystatechange = function () 
+        {
+            if (this.readyState === 4) 
+            {
+                return JSON.parse(this.responseText);
+            }
+        };
+
+        this.request.send();
+    }
+
+    
+}
+
+let animeInfo = new Handler();
